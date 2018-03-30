7449383e0f3a04d2250d159b50032896db15834c
shimazaki <lozld30@gmail.com>
2018-03-30 22:30:18 +0900

    used a3rt api

--- callback.php
+++ callback.php
@@ -17,7 +17,7 @@ $text = $message->{"text"};
 $url = 'https://api.line.me/v2/bot/message/reply';
 $messageData = [
     'type' => 'text',
-    'text' => $text
+    'text' => getTalk($text)
 ];
 $response = [
     'replyToken' => $replyToken,
@@ -44,3 +44,36 @@ curl_setopt($ch, CURLOPT_PROXY, getenv('FIXIE_URL'));
 $result = curl_exec($ch);
 error_log($result);
 curl_close($ch);
+
+// リクルートのAPI(A3RT)
+function getTalk($text) {
+    // A3RT TalkAPI
+    $url = "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk";
+
+    $data = [
+        'apikey' => getenv('API_KEY'),
+        'query' => $text
+    ];
+
+    // セッションを初期化
+    $conn = curl_init();
+
+    // オプション
+    curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($conn, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($conn, CURLOPT_URL, $url);
+    curl_setopt($conn, CURLOPT_POST, true);
+    curl_setopt($conn, CURLOPT_POSTFIELDS, $data);
+
+    // 実行
+    $res = curl_exec($conn);
+
+    // close
+    curl_close($conn);
+
+    //$res = mb_convert_encoding($res,'UTF-8');
+    $obj = json_decode($res, false);
+    $reply = $obj->results[0]->reply;
+    return $reply;
+}
